7d55c3a130ef2c1c23ae9378184460c2944ae5cd
Luiza Alexandru <alexandru97luiza@yahoo.com>
2024-05-30 13:23:10 +0300

    score system

--- basic_functions_needed.py
+++ basic_functions_needed.py
@@ -1,50 +1,100 @@
-import os,time,keyboard
+import os,time,keyboard,random
 import numpy as np
 from values import *
 
 def print_map(map):
+    global p
     os.system('cls')
+    print(intro_text,"\n")
     for row in map:
         print("".join(row))
+    print(f"\n{score}{p}\033[m")
 
 def keyboard_input():
+    global d
     if keyboard.is_pressed('w'):
-        return "up"
+        d='up'
+        return d
     if keyboard.is_pressed('a'):
-        return "left"
+        d='left'
+        return d
     if keyboard.is_pressed('s'):
-        return "down"
+        d='down'
+        return d
     if keyboard.is_pressed('d'):
-        return "right"
+        d='right'
+        return d
+    if keyboard.is_pressed('e'):
+        d='exit'
+        return d
+
+def random_apple():
+    global m,o
+    mapo=np.full((15,35),m)
+    xo=random.randint(1,13)
+    yo=random.randint(1,33)
+    mapo[xo][yo]=o
+    return mapo
+
+def try_print_new_map():
+    global x,y,map,mapo,m,s,o,p
+    map=mapo
+    try:
+        if map[x][y]==o:
+            p+=1
+            mapo=random_apple()
+            map=mapo
+        map[x][y]=s
+    except IndexError:
+        pass
+
+def print_gameover():
+    print(gameover)
+    exit()
 
 def go_up():
-    global x,y,map,m,s
+    global x,y,map,mapo,m,s,o,p,game
+    map[x][y]=m
     x-=1
     if x<0:
-        x=14
-    map=np.full((15,35),m)
-    map[x][y]=s
+        # x=14
+        print_gameover()
+    try_print_new_map()
+    
 
 def go_left():
-    global x,y,map,m,s
+    global x,y,map,mapo,m,s,p,game
+    map[x][y]=m
     y-=1
     if y<0:
-        y=34
-    map=np.full((15,35),m)
-    map[x][y]=s
+        # y=34
+        print_gameover()
+    try_print_new_map()
 
 def go_down():
-    global x,y,map,m,s
+    global x,y,map,mapo,m,s,p,game
+    map[x][y]=m
     x+=1
     if x>14:
-        x=0
-    map=np.full((15,35),m)
-    map[x][y]=s
+        # x=0
+        print_gameover()
+    try_print_new_map()
 
 def go_right():
-    global x,y,map,m,s
+    global x,y,map,mapo,m,s,p,game
+    map[x][y]=m
     y+=1
     if y>34:
-        y=0
-    map=np.full((15,35),m)
-    map[x][y]=s
+        # y=0
+        print_gameover()
+    try_print_new_map()
+
+def where_to_go(d):
+    if d=='up':
+        go_up()
+    if d=='left':
+        go_left()
+    if d=='down':
+        go_down()
+    if d=='right':
+        go_right()
--- values.py
+++ values.py
@@ -1,8 +1,29 @@
 import numpy as np
 
+intro_text="\033[1;37;40mPress or hold 'w''a''s''d' to move up, left, down or right.\nDon't hit the walls and try to get as many apples as you can!\nHold 'e' to exit game.\033[m"
+gameover="\033[1;37;40mGame Over!\033[m"
+goodbye="\033[1;37;40mGoodbye!\033[m"
+score="\033[1;37;40mYour score : "
 m="\033[40;37m \033[m"
 s="\033[40;37mx\033[m"
+o="\033[40;37mo\033[m"
+p=0
 map=np.full((15,35),m)
-map[7][17]=s
 x=7
 y=17
+d=''
+speedx=0.23
+speedy=0.3
+game=True
+
+#color code cheat sheet
+
+# TEXT COLOR	CODE	TEXT STYLE	        CODE	BACKGROUND COLOR	CODE
+# Black	        30	    No effect	        0	    Black	            40
+# Red	        31	    Bold	            1	    Red	                41
+# Green	        32	    Faded   	        2	    Green	            42
+# Yellow	    33	    Italic	            3	    Yellow	            43
+# Blue	        34	    Underline	        4	    Blue	            44
+# Purple	    35		Inverted colors     7       Purple	            45
+# Cyan	        36		Inverted text color 8       Cyan	            46
+# White	        37		Crossed out         9       White	            47
